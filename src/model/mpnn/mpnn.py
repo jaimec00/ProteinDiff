44b4a9abb073f70d166d21cce82252051bf49b03
jaimec00 <hejaca00@gmail.com>
2026-01-08 23:04:00 -0500

    renamed and moved files plus classes
    Signed-off-by: jaimec00 <hejaca00@gmail.com>

---
+++ src/model/mpnn/mpnn.py
@@ -0,0 +1,155 @@
+
+class MPNN(nn.Module):
+	def __init__(self, d_model=256, update_edges=True):
+		super().__init__()
+
+		self.node_mlp = MLP(d_in=3*d_model, d_hidden=d_model, d_out=d_model, hidden_layers=2, act="silu")
+		self.ln1 = nn.LayerNorm(d_model)
+		
+		self.ffn = MLP(d_in=d_model, d_hidden=4*d_model, d_out=d_model, hidden_layers=0, act="silu")
+		self.ln2 = nn.LayerNorm(d_model)
+
+		self._update_edges = update_edges
+		if update_edges:
+			self.edge_mlp = MLP(d_in=3*d_model, d_hidden=d_model, d_out=d_model, hidden_layers=2, act="silu")
+			self.edge_ln = nn.LayerNorm(d_model)
+
+	def forward(self, nodes, edges, nbrs, nbr_mask):
+		
+		nodes = self._node_msg(nodes, edges, nbrs, nbr_mask)
+		edges = self._edge_msg(nodes, edges, nbrs)
+		return nodes, edges
+
+	def _node_msg(self, nodes, edges, nbrs, nbr_mask):
+
+		message = self._create_msg(nodes, edges, nbrs)
+		nodes1 = torch.sum(self.node_mlp(message) * nbr_mask.unsqueeze(-1), dim=1)
+		nodes = self.ln1(nodes + nodes1)
+		nodes = self.ln2(self.ffn(nodes) + nodes)
+		return nodes
+
+	def _edge_msg(self, nodes, edges, nbrs):
+		
+		if self._update_edges:
+			message = self._create_msg(nodes, edges, nbrs)
+			edges = self.edge_ln(edges + self.edge_mlp(message))
+
+		return edges
+
+	def _create_msg(self, nodes, edges, nbrs):
+		ZN, K, D = edges.shape
+		nodes_i = nodes.unsqueeze(1).expand(ZN, K, D)
+		nodes_j = torch.gather(nodes_i, 0, nbrs.unsqueeze(-1).expand(ZN, K, D))
+		message = torch.cat([nodes_i, nodes_j, edges], dim=-1)
+		return message
+
+
+class EdgeEncoder(nn.Module):
+	def __init__(self, d_model=256, top_k=16):
+		super().__init__()
+
+		# rbf stuff (rbfs w/ linearly spaced centers of inter-residue backbone atom pairs)
+		min_rbf, max_rbf, num_rbf = 2.0, 22.0, 16
+		self.top_k = top_k
+		self.register_buffer("rbf_centers", torch.linspace(min_rbf, max_rbf, num_rbf))
+		self.spread = (max_rbf - min_rbf) / num_rbf
+		self.rbf_norm = nn.LayerNorm(num_rbf*4*4)
+		self.rbf_proj = nn.Linear(num_rbf*4*4, d_model)
+
+		# relative frames (flattened 3x3 to 9)
+		self.frame_proj = nn.Linear(9, d_model)
+
+		# relative seq pos (relative difference in sequence)
+		self.seq_emb = nn.Embedding(66, d_model) # [-32,33] 33 is diff chains, 0 is self
+
+		# combine the rbf, frames, and seq emb into a single edge embedding
+		self.edge_mlp = MLP(d_in=d_model*3, d_out=d_model, d_hidden=d_model, hidden_layers=2, act="silu")
+		self.edge_ln = nn.LayerNorm(d_model*3)
+
+	def forward(self, coords_bb, frames, seq_pos, chain_pos, sample_idx):
+
+		nbrs, nbr_mask = self._get_neighbors(coords_bb, sample_idx)
+		edges = self._get_edges(coords_bb, frames, seq_pos, chain_pos, nbrs)
+		return edges, nbrs, nbr_mask
+
+	@torch.no_grad()
+	def _get_neighbors(self, C, sample_idx):
+
+		# prep
+		Ca = C[:, 1, :]
+		ZN, S = Ca.shape
+
+		# get distances
+		dists = torch.sqrt(torch.sum((Ca.unsqueeze(0) - Ca.unsqueeze(1))**2, dim=-1)) # ZN x ZN
+		
+		# sequences from other samples dont count
+		dists.masked_fill_(sample_idx.unsqueeze(0) != sample_idx.unsqueeze(1), float("inf")) # ZN x ZN
+		
+		# get topk 
+		nbrs = dists.topk(self.top_k, dim=1, largest=False) # ZN x K
+
+		# some samples might have less than K tokens, so create a nbr mask
+		nbr_sample_idxs = torch.gather(sample_idx.unsqueeze(1).expand(-1,self.top_k), 0, nbrs.indices)
+		nbr_mask = nbr_sample_idxs == sample_idx.unsqueeze(1)
+
+		# masked edge idxs are the idx corresponding to the self node
+		node_idxs = torch.arange(ZN, device=dists.device).unsqueeze(1) # ZN x 1
+		nbrs = torch.where(nbr_mask, nbrs.indices, node_idxs) # ZN x K
+
+		return nbrs, nbr_mask
+
+	def _get_edges(self, C_backbone, frames, seq_pos, chain_pos, nbrs):
+
+		rel_rbf = self.rbf_proj(self.rbf_norm(self._get_rbfs(C_backbone, nbrs)))
+		rel_frames = self.frame_proj(self._get_frames(frames, nbrs))
+		rel_idxs = self.seq_emb(self._get_seq_pos(seq_pos, chain_pos, nbrs))
+
+		edges = self.edge_mlp(self.edge_ln(torch.cat([rel_rbf, rel_frames, rel_idxs], dim=-1))) # Z,N,K,d_model
+
+		return edges
+
+	@torch.no_grad()
+	def _get_rbfs(self, C_backbone, nbrs):
+
+		ZN, A, S = C_backbone.shape
+		_, K = nbrs.shape
+
+		C_nbrs = torch.gather(C_backbone.unsqueeze(1).expand(ZN, K, A, S), 0, nbrs.reshape(ZN,K,1,1).expand(ZN, K, A, S)) # ZN,K,A,S
+
+		dists = torch.sqrt(torch.sum((C_backbone.reshape(ZN,1,A,1,S) - C_nbrs.reshape(ZN,K,1,A,S))**2, dim=-1)) # ZN,1,A,1,S - ZN,K,1,A,S --> ZN,K,A,A
+
+		rbf_numerator = (dists.reshape(ZN, K, A, A, 1) - self.rbf_centers.reshape(1,1,1,1,-1))**2 # ZN,K,A,A,R
+
+		rbf = torch.exp(-rbf_numerator / (self.spread**2)).reshape(ZN,K,-1)
+		
+		return rbf
+
+	@torch.no_grad()
+	def _get_frames(self, frames, nbrs):
+
+		ZN, K = nbrs.shape
+
+		frames = frames.unsqueeze(1).expand(ZN, K, 3, 3)
+		nbrs = nbrs.reshape(ZN, K, 1, 1).expand(ZN, K, 3, 3)
+		
+		frame_nbrs = torch.gather(frames, 0, nbrs)
+		rel_frames = torch.matmul(frames.transpose(-2,-1), frame_nbrs).reshape(ZN, K, -1)
+
+		return rel_frames
+
+	@torch.no_grad()
+	def _get_seq_pos(self, seq_pos, chain_pos, nbrs):
+
+		ZN, K = nbrs.shape
+
+		seq_pos = seq_pos.unsqueeze(1).expand(ZN, K)		
+		seq_nbrs = torch.gather(seq_pos, 0, nbrs) # ZN,K
+
+		chain_pos = chain_pos.unsqueeze(1).expand(ZN, K)
+		chain_nbrs = torch.gather(chain_pos, 0, nbrs) # ZN,K
+		diff_chain = chain_pos!=chain_nbrs
+
+		rel_idx = torch.clamp(seq_nbrs - seq_pos, min=-32, max=32)
+		rel_idx = rel_idx.masked_fill(diff_chain, 33) + 32 # starts at 0
+
+		return rel_idx
---
+++ src/model/transformer/attention.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+
+from flash_attn.flash_attn_interface import flash_attn_varlen_qkvpacked_func
+
+class FlashMHA(nn.Module):
+	def __init__(self, d_model=256, heads=8, dropout_p=0.0):
+		super().__init__()
+
+		d_k = d_model // heads
+		xavier_scale = (6/(d_k + d_model))**0.5
+
+		self.qkv_proj = nn.Parameter(-xavier_scale + torch.rand(3, heads, d_model, d_k) * (2*xavier_scale)) # 3 x H x Dm x Dk
+		self.qkv_bias = nn.Parameter(torch.zeros(3, heads, d_k)) # 3 x H x Dk
+		self.out_proj = nn.Linear(d_model, d_model, bias=False)
+		self.dropout_p = dropout_p
+
+	def forward(self, x, cu_seqlens, max_seqlen):
+
+		# convenience
+		ZN, Dm = x.shape
+		x_dtype = x.dtype
+		_, H, _, Dk = self.qkv_proj.shape
+
+		# project the tensors
+		QKV = torch.matmul(x.reshape(1,1,ZN,Dm), self.qkv_proj.reshape(3,H,Dm,Dk)) + self.qkv_bias.reshape(3,H,1,Dk) # 1,1,ZN,Dm@3,H,Dm,Dk + 3xHx1xDk->3,H,ZN,Dk
+
+		# reshape and use indices to unpad
+		QKV = QKV.permute(2,0,1,3).to(torch.float16).contiguous() # ZN(valid), 3, H, Dk
+
+		# dropout if in training
+		dropout_p = self.dropout_p if self.training else 0.0
+
+		# flash attention 2
+		out = flash_attn_varlen_qkvpacked_func( # ZN(valid) x H x Dk
+			QKV,
+			cu_seqlens, # q_cu_seqlens, k_cu_seqlens
+			max_seqlen, # q_max_seqlen, k_max_seqlen
+			dropout_p=dropout_p, # dropout
+			softmax_scale=Dk**-0.5, # sm scale
+			deterministic=dropout_p>0.0 # for deterministic bwd, only when dropout is used
+		).to(x_dtype)
+
+		# output projection
+		out = self.out_proj(out.reshape(ZN, Dm))
+
+		return out
