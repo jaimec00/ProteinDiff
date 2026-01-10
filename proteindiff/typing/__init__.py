fb2f1487f741e4a49a90641483d088446977b657
jaimec00 <hejaca00@gmail.com>
2026-01-10 16:32:22 -0500

    add typing module
    Signed-off-by: jaimec00 <hejaca00@gmail.com>

---
+++ proteindiff/typing/__init__.py
@@ -0,0 +1,17 @@
+
+import torch
+T = torch.Tensor
+
+from jaxtyping import Float, Bool, Int, Union
+from typing import List, Dict, Tuple
+
+__all__ = [
+    "T",
+    "Float",
+    "Bool",
+    "Int",
+    "Union",
+    "List",
+    "Dict",
+    "Tuple"
+]
