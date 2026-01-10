30d69074335252511a306e4006ad6de2652a5918
jaimec00 <hejaca00@gmail.com>
2026-01-10 16:30:25 -0500

    add utils
    Signed-off-by: jaimec00 <hejaca00@gmail.com>

---
+++ proteindiff/model/utils/__init__.py
@@ -0,0 +1,13 @@
+from .mlp import (
+    MLP, MLPCfg,
+    MPNNMLP, MPNNMLPCfg,
+    FFN, FFNCfg,
+)
+__all__ = [
+    "MLP", 
+    "MLPCfg"
+    "MPNNMLP", 
+    "MPNNMLPCfg",
+    "FFN", 
+    "FFNCfg",
+]
