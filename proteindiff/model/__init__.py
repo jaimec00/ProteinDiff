97a19e61dd005ba1a872683df91d0bc92ec69e13
jaimec00 <hejaca00@gmail.com>
2026-01-10 16:33:20 -0500

    update imports
    Signed-off-by: jaimec00 <hejaca00@gmail.com>

---
+++ proteindiff/model/__init__.py
@@ -0,0 +1,11 @@
+from .base import Base
+from . import transformer, vae, mpnn, utils, tokenizer
+
+__all__ = [
+    "Base",
+    "transformer",
+    "vae",
+    "mpnn",
+    "utils",
+    "tokenizer",
+]
